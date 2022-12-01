775eea8a3e194c55cc477aeb5142443e78007d99
Odunuga1s <116438182+Odunuga1s@users.noreply.github.com>
2022-12-01 19:02:30 +0000

    Add files via upload

---
+++ habib.py
@@ -0,0 +1,18 @@
+def make_shirt(size, text):
+    print(f'{size, text}')
+
+make_shirt(7,'You\'re the best')
+
+make_shirt(10,'I love Python')
+make_shirt(5, 'I love Python')
+make_shirt(20, 'Believe in yourself')
+
+def describe_city(city, country='USA'):
+    print(f'{city} is in {country}')
+
+describe_city('Miami')
+
+def favourite_book(title):
+    print(f'my favorite book is {title}')
+
+favourite_book('The BFG')
